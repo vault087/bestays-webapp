db8880694878e590f8895a26b410e544b6a2f64e
Andrei Solovev <vault087@gmail.com>
2025-07-24 14:29:48 +0700

    opacity

--- src/components/form/layout/form-field-layout-toolbar.tsx
+++ src/components/form/layout/form-field-layout-toolbar.tsx
@@ -6,7 +6,7 @@ export const FormFieldLayoutToolbar = memo(function FormFieldLayoutToolbar({
   children: React.ReactNode;
 }) {
   return (
-    <div className="absolute top-2 right-3 flex items-center justify-end gap-2 opacity-20 transition-opacity duration-200 group-hover:opacity-100">
+    <div className="absolute top-2 right-3 flex items-center justify-end gap-2 opacity-20 duration-200 group-hover:opacity-100">
       {children}
     </div>
   );
