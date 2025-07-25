8b5817c6cab8c100bfc2c3c2890db7cab0e42363
Andrei Solovev <vault087@gmail.com>
2025-07-25 20:22:43 +0700

    image uploaded

--- supabase/migrations/preparation/20_property_images_bucket.sql
+++ supabase/migrations/preparation/20_property_images_bucket.sql
@@ -1,2 +1,6 @@
 CREATE POLICY "Allow authenticated users to upload to my_private_bucket"
 ON storage.objects FOR INSERT TO authenticated WITH CHECK (bucket_id = 'property-images');
+
+-- Add policy to allow reading files (required for signed URLs)
+CREATE POLICY "Allow public read access to property images"
+ON storage.objects FOR SELECT TO public USING (bucket_id = 'property-images');
