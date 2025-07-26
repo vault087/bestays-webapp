7e5a5b786d2ca5516fe379ece945a82684b4194f
Andrei Solovev <vault087@gmail.com>
2025-07-26 22:54:17 +0700

    turned off middleware

--- src/app/[locale]/(dashboard)/layout.tsx
+++ src/app/[locale]/(dashboard)/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
+import { redirect } from "next/navigation";
 import RootLayout from "@/components/layout/root-layout";
+import { getUser } from "@/entities/users/user.libs";
 
 export const metadata: Metadata = {
   title: "Bestays",
@@ -13,6 +15,12 @@ export default async function DashboardLayout({
   params: Promise<{ locale: string }>;
   children: React.ReactNode;
 }>) {
+  const user = await getUser();
+
+  if (!user) {
+    redirect("/login");
+  }
+
   return (
     <RootLayout params={params}>
       <div className="flex min-h-screen flex-col">{children}</div>
